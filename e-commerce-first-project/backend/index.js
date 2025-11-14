67ffe8062873e37ab64f6e9436daa19b7d89a599
im-lakshyaa <lakshya20042004@gmail.com>
2025-11-14 14:55:30 +0530

    backend-done

---
+++ e-commerce-first-project/backend/index.js
@@ -0,0 +1,29 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import AuthController from "../backend/authController.js";
+import express from "express";
+dotenv.config();
+const PORT = 3000;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+
+app.get("/",  (req, res) => {
+    res.send("hello world");
+})
+
+app.use("/api/auth",AuthController)
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- e-commerce-first-project/src/components/Home.jsx
+++ e-commerce-first-project/src/components/Home.jsx
@@ -1,31 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Card from "./Card.jsx";
-import products from "../product-api/product.js";
 import { useSearch } from "../context/SearchContext";
 
 const Home = () => {
   const { search } = useSearch();
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  // Filter products based on search text
+  
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await axios.get("https://dummyjson.com/products");
+        setProducts(res.data.products); 
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+
   const filteredProducts = products.filter((product) =>
     product.title.toLowerCase().includes(search.toLowerCase())
   );
 
   return (
     <div className="p-4 lg:p-8">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 justify-items-center">
-
-        {filteredProducts.length > 0 ? (
-          filteredProducts.map((product) => (
-            <Card key={product.id} productObj={product} />
-          ))
-        ) : (
-          <p className="text-gray-600 text-lg col-span-full text-center">
-            No products found for "{search}"
-          </p>
-        )}
-
-      </div>
+      {loading ? (
+        <p className="text-center text-lg text-gray-600">Loading products...</p>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 justify-items-center">
+          {filteredProducts.length > 0 ? (
+            filteredProducts.map((product) => (
+              <Card key={product.id} productObj={product} />
+            ))
+          ) : (
+            <p className="text-gray-600 text-lg col-span-full text-center">
+              No products found for "{search}"
+            </p>
+          )}
+        </div>
+      )}
     </div>
   );
 };
