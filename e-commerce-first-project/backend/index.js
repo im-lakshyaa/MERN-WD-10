e07efb42bf4d68c76647823346721aea12aca839
im-lakshyaa <lakshya20042004@gmail.com>
2025-11-22 20:16:05 +0530

    changes done

--- e-commerce-first-project/backend/index.js
+++ e-commerce-first-project/backend/index.js
@@ -8,7 +8,11 @@ const PORT = 3000;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 
 mongoose
